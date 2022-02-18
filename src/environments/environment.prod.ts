06c6f58a8a2b2fb313b53c70f3d7c2ba95ba6a16
Francisco Briones <franciscobrioneslavados@gmail.com>
2022-02-18 13:35:39 -0300

    try to private

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true,
+  firebase_config: {
+    apiKey: 'AIzaSyD8gJjQ-J84Q3An5lh_yBFrCaiXW9-ru7U',
+    authDomain: 'personal-resume-36dbe.firebaseapp.com',
+    projectId: 'personal-resume-36dbe',
+    storageBucket: 'personal-resume-36dbe.appspot.com',
+    messagingSenderId: '179325379274',
+    appId: '1:179325379274:web:3b587e29bbfee5c150ca33'
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,14 @@
 
 export const environment = {
   production: false,
+  firebase_config: {
+    apiKey: "AIzaSyD8gJjQ-J84Q3An5lh_yBFrCaiXW9-ru7U",
+    authDomain: "personal-resume-36dbe.firebaseapp.com",
+    projectId: "personal-resume-36dbe",
+    storageBucket: "personal-resume-36dbe.appspot.com",
+    messagingSenderId: "179325379274",
+    appId: "1:179325379274:web:3b587e29bbfee5c150ca33"
+  },
 };
 
 /*
